eca2d8db55e92872e04d8e37a7161acadd79ad64
Tobias Rummelsberger <tobias.rummelsberger@sennder.com>
2020-12-05 17:31:04 +0100

    Formatted and added NOT NULL constraint for FOREIGN key columns
    Formatted code according to pep8

--- etl.py
+++ etl.py
@@ -8,27 +8,38 @@ from sql_queries import *
 def process_song_file(cur, filepath):
     """
     Processes the individual song files in json format.
-    
+
     - Read and process song data.
-    
+
     - Read and process artists data.
     """
 
     # open song file
-    df = pd.read_json(filepath, typ='series')
+    df = pd.read_json(filepath, typ="series")
 
     # insert song record
-    song_data = song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values.tolist()
+    song_data = song_data = df[
+        ["song_id", "title", "artist_id", "year", "duration"]
+    ].values.tolist()
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist recordo
-    artist_data = artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values.tolist()
-    cur.execute(artist_table_insert, artist_data)    
+    artist_data = artist_data = df[
+        [
+            "artist_id",
+            "artist_name",
+            "artist_location",
+            "artist_latitude",
+            "artist_longitude",
+        ]
+    ].values.tolist()
+    cur.execute(artist_table_insert, artist_data)
+
 
 def bulk_insert(cur, df, create_tmp_table, tmp_table, bulk_insert):
     """
     Bulk inserts dataframe to destination table.
-    
+
     - Creates temporary csv
 
     - Opens csv and copys into the database table
@@ -36,14 +47,14 @@ def bulk_insert(cur, df, create_tmp_table, tmp_table, bulk_insert):
     - Delets temporary csv
     """
 
-    tmp_csv='./tmp.csv'
+    tmp_csv = "./tmp.csv"
     # create temporary csv to use bulk insert to database
-    df.to_csv(tmp_csv, header=False, index=False, sep='\t')
-    f = open(tmp_csv, 'r')
+    df.to_csv(tmp_csv, header=False, index=False, sep="\t")
+    f = open(tmp_csv, "r")
 
     # create temp table in order to not violate unique constraint
     cur.execute(create_tmp_table)
-    cur.copy_from(f, tmp_table, sep='\t')
+    cur.copy_from(f, tmp_table, sep="\t")
     cur.execute(bulk_insert)
 
     # Remove temporary csv
@@ -53,7 +64,7 @@ def bulk_insert(cur, df, create_tmp_table, tmp_table, bulk_insert):
 def process_log_file(cur, filepath):
     """
     Processes log file and bulk inserts into users, time and songplays.
-    
+
     - Reads json log file
 
     - Transforms and inserts time data in batches
@@ -68,31 +79,78 @@ def process_log_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = df[df['page']=='NextSong']
-    
-    df['ts_raw'] = df['ts']
-    df['ts'] = pd.to_datetime(df['ts'], unit='ms')
+    df = df[df["page"] == "NextSong"]
+
+    df["ts_raw"] = df["ts"]
+    df["ts"] = pd.to_datetime(df["ts"], unit="ms")
 
     # convert timestamp column to datetime
-    t = df['ts']
-    
+    t = df["ts"]
+
     # insert time data records
-    time_data = [t, t.dt.hour, t.dt.day, t.dt.isocalendar().week, t.dt.month, t.dt.year, t.dt.weekday]
-    column_labels = ('timestamp', 'hour', 'day', 'week of year', 'month', 'year', 'weekday')
+    time_data = [
+        t,
+        t.dt.hour,
+        t.dt.day,
+        t.dt.isocalendar().week,
+        t.dt.month,
+        t.dt.year,
+        t.dt.weekday,
+    ]
+    column_labels = (
+        "timestamp",
+        "hour",
+        "day",
+        "week of year",
+        "month",
+        "year",
+        "weekday",
+    )
     time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
-    bulk_insert(cur=cur, df=time_df, create_tmp_table=create_tmp_time_table, tmp_table='tmp_time', bulk_insert=time_table_bulk_insert)
+    bulk_insert(
+        cur=cur,
+        df=time_df,
+        create_tmp_table=create_tmp_time_table,
+        tmp_table="tmp_time",
+        bulk_insert=time_table_bulk_insert,
+    )
 
     # load user table
-    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
-    bulk_insert(cur=cur, df=user_df, create_tmp_table=create_tmp_users_table, tmp_table='tmp_users', bulk_insert=users_table_bulk_insert)
+    bulk_insert(
+        cur=cur,
+        df=user_df,
+        create_tmp_table=create_tmp_users_table,
+        tmp_table="tmp_users",
+        bulk_insert=users_table_bulk_insert,
+    )
 
     # create temporary csv to use bulk insert to database
 
-    songplay_df = df[['ts', 'userId', 'level', 'sessionId', 'location', 'userAgent', 'song', 'artist', 'length']]
-    
-    bulk_insert(cur=cur, df=songplay_df, create_tmp_table=create_tmp_songplays_table, tmp_table='tmp_songplays', bulk_insert=songplays_table_bulk_insert)
+    songplay_df = df[
+        [
+            "ts",
+            "userId",
+            "level",
+            "sessionId",
+            "location",
+            "userAgent",
+            "song",
+            "artist",
+            "length",
+        ]
+    ]
+
+    bulk_insert(
+        cur=cur,
+        df=songplay_df,
+        create_tmp_table=create_tmp_songplays_table,
+        tmp_table="tmp_songplays",
+        bulk_insert=songplays_table_bulk_insert,
+    )
+
 
 def process_data(cur, conn, filepath, func):
     """
@@ -102,44 +160,62 @@ def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print("{} files found in {}".format(num_files, filepath))
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+        print("{}/{} files processed.".format(i, num_files))
+
 
 def quality_check(cur, conn):
     """
     Runs some basic quality checks on data consistency.
     """
-    
+
     # check for year that is null
-    cur.execute("SELECT COUNT(*) FROM songs WHERE year IS NULL OR year < 1900;")
-    print("{} distinct songs where year is invalid. ".format(cur.fetchone()[0]))
-    
+    cur.execute(
+        "SELECT COUNT(*) FROM songs WHERE year IS NULL OR year < 1900;"
+        )
+    print(
+        "{} distinct songs where year is invalid. ".format(cur.fetchone()[0])
+        )
+
     # check for empty artist
-    cur.execute("SELECT COUNT(*) FROM artists WHERE name IS NULL;")
-    print("{} distinct artists where name is invalid. ".format(cur.fetchone()[0]))
+    cur.execute(
+        "SELECT COUNT(*) FROM artists WHERE name IS NULL;"
+        )
+    print(
+        "{} distinct artists where name is invalid.".format(cur.fetchone()[0])
+    )
 
     # check for songplay that has incomplete information
-    cur.execute("SELECT COUNT(*) FROM songplays WHERE artist_id IS NULL or songplay_id IS NULL")
-    print("{} distinct songplays with incomplete information. ".format(cur.fetchone()[0]))
+    cur.execute(
+        "SELECT COUNT(*) \
+        FROM songplays \
+        WHERE artist_id IS NULL or songplay_id IS NULL"
+    )
+    print(
+        "{} distinct songplays with incomplete information. "
+        .format(cur.fetchone()[0])
+    )
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student"
+    )
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath="data/song_data", func=process_song_file)
+    process_data(cur, conn, filepath="data/log_data", func=process_log_file)
 
     quality_check(cur, conn)
 
--- sql_queries.py
+++ sql_queries.py
@@ -8,21 +8,21 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = """
 CREATE TABLE songplays (
     songplay_id SERIAL PRIMARY KEY, 
     start_time TIMESTAMP, 
-    user_id VARCHAR, 
+    user_id VARCHAR NOT NULL, 
     level VARCHAR, 
-    song_id VARCHAR, 
-    artist_id VARCHAR, 
-    session_id VARCHAR, 
+    song_id VARCHAR NOT NULL, 
+    artist_id VARCHAR NOT NULL, 
+    session_id VARCHAR NOT NULL, 
     location VARCHAR, 
     user_agent VARCHAR
 )
-""")
+"""
 
-user_table_create = ("""
+user_table_create = """
 CREATE TABLE users (
     user_id VARCHAR PRIMARY KEY, 
     first_name VARCHAR, 
@@ -30,19 +30,19 @@ CREATE TABLE users (
     gender VARCHAR, 
     level VARCHAR
 )
-""")
+"""
 
-song_table_create = ("""
+song_table_create = """
 CREATE TABLE songs (
     song_id VARCHAR PRIMARY KEY, 
     title VARCHAR, 
-    artist_id VARCHAR, 
-    year INT, 
+    artist_id VARCHAR NOT NULL, 
+    year INT NOT NULL, 
     duration NUMERIC
 )
-""")
+"""
 
-artist_table_create = ("""
+artist_table_create = """
 CREATE TABLE artists (
     artist_id VARCHAR PRIMARY KEY, 
     name VARCHAR, 
@@ -50,92 +50,168 @@ CREATE TABLE artists (
     latitude NUMERIC, 
     longitude NUMERIC
 )
-""")
+"""
 
-time_table_create = ("""
+time_table_create = """
 CREATE TABLE time (
     start_time TIMESTAMP PRIMARY KEY, 
-    hour INT, 
-    day INT, 
-    week INT, 
-    month INT, 
-    year INT, 
-    weekday INT
+    hour INT NOT NULL, 
+    day INT NOT NULL, 
+    week INT NOT NULL, 
+    month INT NOT NULL, 
+    year INT NOT NULL, 
+    weekday INT NOT NULL
 )
-""")
+"""
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+songplay_table_insert = """
+INSERT INTO songplays (
+    start_time, 
+    user_id, 
+    level, 
+    song_id, 
+    artist_id, 
+    session_id, 
+    location, 
+    user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING;
-""")
+"""
 
-create_tmp_songplays_table = ("""
-CREATE TEMP TABLE tmp_songplays (ts TIMESTAMP, userId VARCHAR, level VARCHAR, sessionId VARCHAR, location VARCHAR, 
-userAgent VARCHAR, song 
-VARCHAR, artist VARCHAR, length NUMERIC) ON COMMIT DROP;
-""")
+create_tmp_songplays_table = """
+CREATE TEMP TABLE tmp_songplays (
+    ts TIMESTAMP, 
+    userId VARCHAR, 
+    level VARCHAR, 
+    sessionId VARCHAR, 
+    location VARCHAR, 
+    userAgent VARCHAR, 
+    song VARCHAR, 
+    artist VARCHAR, 
+    length NUMERIC) 
+ON COMMIT DROP;
+"""
 
-songplays_table_bulk_insert = ("""
-INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
-(SELECT tmp_songplays.ts, tmp_songplays.userId, tmp_songplays.level, songs.song_id, artists.artist_id, tmp_songplays.sessionId, tmp_songplays.location, tmp_songplays.userAgent FROM tmp_songplays 
-LEFT JOIN songs ON tmp_songplays.song = songs.title AND tmp_songplays.length = songs.duration
-LEFT JOIN artists ON tmp_songplays.artist = artists.name)
+songplays_table_bulk_insert = """
+INSERT INTO songplays (
+    start_time, 
+    user_id, 
+    level, 
+    song_id, 
+    artist_id, 
+    session_id, 
+    location, 
+    user_agent) 
+(SELECT 
+    tmp_songplays.ts, 
+    tmp_songplays.userId, 
+    tmp_songplays.level, 
+    songs.song_id, 
+    artists.artist_id, 
+    tmp_songplays.sessionId, 
+    tmp_songplays.location, 
+    tmp_songplays.userAgent 
+FROM tmp_songplays 
+LEFT JOIN songs ON tmp_songplays.song = songs.title 
+    AND tmp_songplays.length = songs.duration
+LEFT JOIN artists ON tmp_songplays.artist = artists.name
+WHERE songs.song_id IS NOT NULL AND artists.artist_id IS NOT NULL)
 ON CONFLICT DO NOTHING;
 DROP TABLE IF EXISTS tmp_songplays;
-""")
+"""
 
-user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING;
-""")
+user_table_insert = """
+INSERT INTO users (
+    user_id, 
+    first_name, 
+    last_name, 
+    gender, 
+    level) 
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO UPDATE SET level = %s;
+"""
 
-create_tmp_users_table = ("""
+create_tmp_users_table = """
 CREATE TEMP TABLE tmp_users (LIKE users) ON COMMIT DROP;
-""")
+"""
 
-users_table_bulk_insert = ("""
-INSERT INTO users SELECT DISTINCT ON (user_id) * FROM tmp_users ON CONFLICT DO NOTHING;
+users_table_bulk_insert = """
+INSERT INTO users 
+    SELECT DISTINCT ON (user_id) * FROM tmp_users 
+    ON CONFLICT (user_id) DO UPDATE SET level = excluded.level;
 DROP TABLE IF EXISTS tmp_users;
-""")
+"""
 
-song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
+song_table_insert = """
+INSERT INTO songs (
+    song_id, 
+    title, 
+    artist_id, 
+    year, 
+    duration) 
+VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING;
-""")
+"""
 
-artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
+artist_table_insert = """
+INSERT INTO artists (
+    artist_id, 
+    name, 
+    location, 
+    latitude, 
+    longitude) 
+VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING;
-""")
+"""
 
-time_table_insert = ("""
-INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
+time_table_insert = """
+INSERT INTO time (
+    start_time, 
+    hour, 
+    day, 
+    week, 
+    month, 
+    year, 
+    weekday) 
+VALUES (%s, %s, %s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING;
-""")
+"""
 
-create_tmp_time_table = ("""
+create_tmp_time_table = """
 CREATE TEMP TABLE tmp_time (LIKE time) ON COMMIT DROP;
-""")
+"""
 
-time_table_bulk_insert = ("""
-INSERT INTO time SELECT DISTINCT ON (start_time) * FROM tmp_time ON CONFLICT DO NOTHING;
+time_table_bulk_insert = """
+INSERT INTO time 
+SELECT DISTINCT ON (start_time) * FROM tmp_time ON CONFLICT DO NOTHING;
 DROP TABLE IF EXISTS tmp_time;
-""")
+"""
 
 
 # FIND SONGS
 
-song_select = ("""
+song_select = """
 SELECT songs.song_id, songs.artist_id 
 FROM songs 
 LEFT JOIN artists ON songs.artist_id = artists.artist_id 
 WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s ;
-""")
+"""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create,
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop,
+]
